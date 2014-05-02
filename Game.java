8c33cbbde6b42202c3c54c29c10042bcfe502c56
SergioA <sergioalvarez316@gmail.com>
2014-05-02 10:56:00 +0200

    Añadido el comando look a a clase commandWord y Game,que se utiliza para ver lo que hay en una localización

--- Game.java
+++ Game.java
@@ -118,6 +118,9 @@ public class Game
         else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
         }
+        else if (commandWord.equals("look")){
+             currentRoom.getLongDescription();
+        }
 
         return wantToQuit;
     }
@@ -135,7 +138,7 @@ public class Game
         System.out.println("around at the university.");
         System.out.println();
         System.out.println("Your command words are:");
-        System.out.println("   go quit help");
+        System.out.println("   go quit help look");
     }
 
     /** 
